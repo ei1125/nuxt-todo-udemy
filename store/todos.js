d46a98dd8bb808ad31a97a2250be9a4fdc028731
ei1125 <i.dont.like.monday1125@gmail.com>
2020-08-15 21:36:32 +0900

    todos.jsの作成

---
+++ store/todos.js
@@ -0,0 +1,34 @@
+import firebase from '~/plugins/firebase'
+import { firestoreAction } from 'vuexfire'
+
+const db = firebase.firestore()
+const todosRef = db.collection('todo')
+
+export const state = () => ({
+  todos: []
+})
+
+export const actions = {
+  init: firestoreAction(({ bindFirestoreRef }) => {
+    bindFirestoreRef('todos', todosRef)
+  }),
+  add: firestoreAction((context, name) => {
+    // nameが空白でないことを確認
+    if(name.trim()) {
+      todosRef.add({
+        name: name,
+        done: false,
+        created: firebase.firestore.FieldValue.serverTimestamp()
+      })
+    }
+  }),
+  remove: firestoreAction((context, id) => {
+    todosRef.doc(id).delete()
+  }),
+  toggle: firestoreAction((context, todo) => {
+    todosRef.doc(todo.id).update({
+      // 現在の値を反転して登録
+      done: !todo.done
+    })
+  })
+}
